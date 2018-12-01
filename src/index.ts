6b0343219c1b349d0a8e5b8d06373c8b9b300be6
Hernan Rajchert <hrajchert@gmail.com>
2018-11-30 21:25:38 -0300

    feat(poc): make work the initial example

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,38 @@
 import { dependencies } from '@ts-task/fetch';
 import rest from './pet-spec';
+import { json } from './restify';
 // Set node-fetch as a way to fetch in task-fetch
 dependencies.fetch = require('node-fetch');
 
-const pets = rest.get('/pets', {
-    queryParams: {
-        limit: 20,
-        tags: ['lazy']
-    }
-});
 
-pets.fork(
-    err => console.log('Oh noo', err.message),
-    data => console.log('Ehhhh....', data)
-);
 
 rest.get('/pets/{petId}', {
     pathParams: {
-        petId: 28
+        petId: 1
     }
-});
+}).fork(
+    err => console.log('/pets/{petId} Oh noo', err.message),
+    data => console.log('/pets/{petId} Ehhhh....', data)
+);
 
 const ping = rest.get('/ping');
 
+
 rest.post('/pets', {
-    requestBody: {
-        name: 'vilma'
-    }
-});
+        body: json({
+            name: 'vilma'
+        })
+    })
+    .map(res => console.log('post', res))
+    .chain(
+        _ => rest.get('/pets', {
+            queryParams: {
+                limit: 20,
+                tags: ['lazy']
+            }
+        })
+    )
+    .fork(
+        err => console.log('post and get error:', err.message),
+        data => console.log('post and get success:', data)
+);
--- src/restify.ts
+++ src/restify.ts
@@ -1,10 +1,42 @@
 import { fetch } from '@ts-task/fetch';
 import { Task } from '@ts-task/task';
 
-interface RestifyEndpoints {
-    get: any;
-    post: any;
-    put: any;
+export interface Stringable {
+    toString (): string;
+}
+
+export type EndpointSpecOptions = {
+    queryParams?: {
+        [prop: string]: unknown;
+    };
+    pathParams?: {
+        [prop: string]: Stringable;
+    };
+    body?: {
+        contentType: string;
+        data: {
+            [prop: string]: unknown;
+        }
+    };
+} | undefined;
+
+export interface EndpointSpec {
+    options: EndpointSpecOptions;
+
+    responses: {
+        code: number;
+        json: unknown;
+    };
+}
+
+interface EndpointSpecs {
+    [path: string]: EndpointSpec;
+}
+
+export interface RestifyEndpoints {
+    get: EndpointSpecs;
+    post: EndpointSpecs;
+    put: EndpointSpecs;
 }
 
 interface RestifyOptions {
@@ -12,15 +44,60 @@ interface RestifyOptions {
 }
 
 type Endpoint <Spec extends RestifyEndpoints, Method extends Methods>
-    = <Route extends PossibleEndpoints<Spec, Method>> (url: Route, ...options: EndpointOptions<Spec, Method, Route>) => EndpointResponse<Spec, Method, Route>;
+    = <Route extends PossibleEndpoints<Spec, Method>> (url: Route, ...options: EndpointOptions<Spec, Method, Route>) => EndpointResponse<Spec, Method, Route>
+;
+
+
+function replacePath (path: string, options: EndpointSpecOptions) {
+    // If we don't have options or pathParams, return as is
+    if (!options || !options.pathParams) return path;
+
+    // Get the indexes to replace
+    const params = options.pathParams;
+    const indexes = Object.keys(options.pathParams);
+
+    // Replace each index with it's value
+    return indexes.reduce(
+        (path, index) => path.replace(`{${index}}`, params[index].toString()),
+        path
+    );
+}
+
+function normalizeBody (options: EndpointSpecOptions): BodyInit | null {
+    if (!options || !options.body) return null;
+    if (options.body.contentType === 'application/json') {
+        return JSON.stringify(options.body.data);
+    } else {
+        throw new Error(`content type ${options.body.contentType} not implemented`);
+    }
+}
+
+export function json <T> (data: T) {
+    return {
+        contentType: 'application/json' as 'application/json',
+        data
+    };
+}
+
+function normalizeHeaders (options: EndpointSpecOptions): HeadersInit {
+    if (!options) return {};
+
+    const contentType: Record<string, string> =
+        options.body ? {'Content-Type': options.body.contentType} : {};
+
+    return {
+        ...contentType
+    };
+}
 
 export function restify<Spec extends RestifyEndpoints> (restifyOptions: RestifyOptions) {
     function request (method: Methods) {
-        return function (path: string, options: any) {
-            // TODO: replace path
-            const url = restifyOptions.servers + path;
+        return function (path: string, options: EndpointSpecOptions) {
+            const url = restifyOptions.servers + replacePath(path, options);
             return fetch(url, {
-                method
+                method,
+                body: normalizeBody(options),
+                headers: normalizeHeaders(options)
             }).chain(res => res.json());
         };
 
